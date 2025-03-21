369dc9337122503c36f3fe5b0bf7320a47b8e9fc
jason tsai <jason.tsaicc@gmail.com>
2025-03-21 17:02:55 +0800

    Add AWS infrastructure components: internet gateway, route table, and update Jenkins installation script

--- main.tf
+++ main.tf
@@ -12,6 +12,23 @@ resource "aws_subnet" "public" {
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = true
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
 
 resource "aws_security_group" "jenkins_sg" {
     name = "jenkins_sg"
@@ -50,16 +67,16 @@ resource "aws_instance" "jenkins_ec2" {
     key_name = "jenkins_test"
      user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              amazon-linux-extras install java-openjdk11 -y
-              wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-              rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-              yum install jenkins git -y
-              systemctl enable jenkins
-              systemctl start jenkins
+              sudo yum update -y
+              sudo dnf install java-17-amazon-corretto -y
+              sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+              sudo dnf install jenkins -y
+              sudo systemctl enable jenkins
+              sudo systemctl start jenkins
               EOF
     tags = {
-        Name = "jenkins"
+        Name = "jenkins_ec2"
     }
   
 }
--- output.tf
+++ output.tf
@@ -1,7 +1,17 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
-  
+  value = aws_vpc.main.cidr_block
 }
-output "ec2" {
+output "subnet_id_public_cidr_block" {
+  value = aws_subnet.public.cidr_block
+}
+
+output "jenkins_ec2_availability_zone" {
+  value = aws_instance.jenkins_ec2.availability_zone
+}
+output "jenkins_ec2_private_ip" {
+  value = aws_instance.jenkins_ec2.private_ip
+}
+output "jenkins_ec2_public_ip" {
   value = aws_instance.jenkins_ec2.public_ip
 }
+
