84a16817f8fbd32bf5b84a601712db96d9ef6379
jason tsai <jason.tsaicc@gmail.com>
2025-03-22 21:15:18 +0800

    Add outputs for Jenkins EC2 instance IPs and availability zone in Terraform configuration

--- output.tf
+++ output.tf
@@ -1,6 +1,7 @@
 output "vpc_id" {
   value = aws_vpc.main.cidr_block
 }
+
 output "subnet_id_public_cidr_block" {
   value = aws_subnet.public.cidr_block
 }
@@ -8,9 +9,11 @@ output "subnet_id_public_cidr_block" {
 output "jenkins_ec2_availability_zone" {
   value = aws_instance.jenkins_ec2.availability_zone
 }
+
 output "jenkins_ec2_private_ip" {
   value = aws_instance.jenkins_ec2.private_ip
 }
+
 output "jenkins_ec2_public_ip" {
   value = aws_instance.jenkins_ec2.public_ip
 }
